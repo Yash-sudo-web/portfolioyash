6e2cf6e1c97a0624573ff6aca2e15ef14bda4004
Yash-sudo-web <ymathur123@gmail.com>
2024-01-18 15:24:54 +0530

    hero complete

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import hero from '../assets/hero.jpg'
-import hero2 from '../assets/hero2.jpeg'
-import hero3 from '../assets/hero3.jpg'
+import React from 'react';
+import hero from '../assets/hero.jpg';
+
 export const Hero = () => {
   return (
     <>
-    <div className='m-auto max-w-[1176px] flex'>
+      <div className='relative m-auto max-w-[1176px] flex'>
+        <div className='absolute bg-[#f7f8f2] text-black z-30 text-5xl top-[57.5%] font-bold'>
+          STRENGTH <br /> IN SIMPLICITY
+        </div>
         <div>
-            <img src={hero} className='h-[70%] w-[90%] grayscale border-l-2 border-r-2 border-b-2 border-t' />
+          <img src={hero} className='h-[67.5%] w-[90%] grayscale border-l-2 border-r-2 border-b-2 border-t' alt='Hero' />
         </div>
+
         <div className='w-[40%] text-white'>
-            <br/><br/><br/>
-            <p className='text-[22px]'>An architectural style that emerged in the mid-20s is based in raw and unadorned use of concrete.</p>
-            <br/>
-            <p className='text-lg font-nova-flat'>Named by the French term béton brut, or raw concrete, Brutalism became popular in the post-WWII era, when the need for affordable housing and public buildings led to the widespread use of concrete.</p>
+          <br/><br/><br/>
+          <p className='text-[22px]'>An architectural style that emerged in the mid-20s is based on the raw and unadorned use of concrete.</p>
+          <br/>
+          <p className='text-lg font-nova-flat'>Named by the French term béton brut, or raw concrete, Brutalism became popular in the post-WWII era, when the need for affordable housing and public buildings led to the widespread use of concrete.</p>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
